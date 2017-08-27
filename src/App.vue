d9fa349f349a903829f1ed61c800e016aee62980
kobakan618 <kobakan618@ezweb.ne.jp>
2017-08-28 00:27:54 +0900

    First Modify for umayado

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,31 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <!--ナビゲーションバー-->
+    <nav class="navbar navbar-inverse navbar-fixed-top">
+      <div class="container-fluid">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbarEexample3">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">
+            UMAYADO
+          </a>
+        </div>
+        <div class="collapse navbar-collapse" id="navbarEexample3">
+          <form class="navbar-form navbar-left" role="search">
+            <div class="form-group">
+              <input type="text" class="form-control" placeholder="名前を入力して下さい">
+            </div>
+            <button type="button" class="btn btn-primary start-button">
+              <i class="start-text"></i>
+            </button>
+          </form>
+        </div>
+      </div>
+    </nav>
     <router-view></router-view>
   </div>
 </template>
@@ -9,6 +34,7 @@
 export default {
   name: 'app'
 }
+
 </script>
 
 <style>
